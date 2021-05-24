49e7495711cb2a0f37fcb7c7a080d5260f3cc499
Thomas Steiner <thomas.steiner.private@gmail.com>
2021-05-25 00:41:12 +0200

    added creating game local

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const butInstall = document.getElementById("butInstall");
 const butSend = document.getElementById("butSend");
 const gamesContainer = document.getElementById("games");
+const createGame = document.getElementById('create_new_game');
+const newGame = document.getElementById('new_game');
+const addPlayer = document.getElementById('add_player_button');
+const newPlayer = document.getElementById('new_player');
+const players = document.getElementById('players');
+const submitButton = document.getElementById('submit_game');
 
 let defferredPrompt;
 
@@ -82,6 +88,7 @@ navigator.serviceWorker.addEventListener("controllerchange", () => {
 
 const getGames = async () => {
   try {
+    // const games = await fetch('http://localhost:1337/games');
     const games = await fetch('https://gamblr-api.herokuapp.com/games');
     return games.json();
   } catch (e) {
@@ -89,16 +96,22 @@ const getGames = async () => {
   }
 }
 
-const allGames = getGames().then((res) => {
-  const elements = res.map((element) => {
-    const players = Object.entries(element.results).map(([key, value]) => (`
+const renderHTML = (id, content) => {
+  const domElement = document.getElementById(id);
+  domElement.innerHTML += content;
+}
+
+const renderGames = () => {
+  getGames().then((res) => {
+    const elements = res.map((element) => {
+      const players = Object.entries(element.results).map(([key, value]) => (`
     <tr>
       <td>${key}</td>
       <td>${value}</td>
     </tr>
     `)).join('');
 
-    return (`
+      return (`
       <table class="game">
         <thead>
           <tr>
@@ -110,8 +123,62 @@ const allGames = getGames().then((res) => {
         ${players}
         </tfoot>
       </table>`)
+    });
+    gamesContainer.innerHTML += elements.join(' ');
   });
-  gamesContainer.innerHTML += elements.join(' ');
+};
+
+createGame.addEventListener('click', () => {
+  newGame.style.display = 'inline';
 });
 
+addPlayer.addEventListener('click', () => {
+  renderHTML('players',
+    `
+    <tr id="${newPlayer.value}_container">
+      <td id="${newPlayer.value}_name">${newPlayer.value}</td>
+      <td id="${newPlayer.value}_points">0</td>
+      <td>
+        <button id="${newPlayer.value}_add">+</button>
+      </td>
+      <td>
+        <button id="${newPlayer.value}_remove">-</button>
+      </td>
+    </tr>
+  `);
+});
 
+players.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    const idArray = e.target.id.split('_');
+    const [name, type] = idArray;
+    console.log(name, type);
+    const valueContainer = document.getElementById(`${name}_points`);
+    const currentValue = parseInt(valueContainer.innerHTML, 10);
+    if (type === 'add') {
+      currentValue < 3 ? valueContainer.innerHTML = currentValue + 1 : valueContainer.innerHTML = currentValue;
+    } else if (type === 'remove') {
+      currentValue > 1 ? valueContainer.innerHTML = currentValue - 1 : valueContainer.innerHTML = 0;
+    }
+  }
+});
+
+const submitGame = () => {
+  const allPlayers = document.getElementById('players');
+  console.log(allPlayers.children);
+  const results = {};
+  for (player of allPlayers.children) {
+    const name = player.id.split('_')[0];
+    const value = document.getElementById(`${name}_points`).innerHTML;
+    results[name] = value;
+  }
+  console.log('RES', results);
+};
+
+submitButton.addEventListener('click', () => {
+  submitGame();
+});
+
+window.addEventListener('load', () => {
+  renderGames();
+});

3189b3b0bad4f439e464e1dd95a1de5373caad92
Thomas Steiner <thomas.steiner.private@gmail.com>
2021-05-26 21:03:04 +0200

    changed notification

--- sw.js
+++ sw.js
@@ -52,9 +52,9 @@ self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push Received.');
   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-  const title = 'Push Codelab';
+  const title = 'Gamblr Notification';
   const options = {
-    body: 'Yay it works.',
+    body: event.data.text(),
     icon: 'images/icon.png',
     badge: 'images/badge.png'
   };
